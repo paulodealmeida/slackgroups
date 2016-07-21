f7ad60bee1959eb2d1c3290319609ae2afded4ec
Rafael Gil <rafael@dagobah.xmsistemas.com.br>
2016-07-21 15:34:12 +0000

    Initial commit of the front-end project

---
+++ front-end/src/main.jsx
@@ -0,0 +1,5 @@
+import Slackgroups from './slackgroups';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<Slackgroups />, document.querySelector('#view'));
---
+++ front-end/src/slackgroups.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Slackgroups extends React.Component {
+  render() {
+    return <div>Welcome to Slackgroups!</div>;
+  }
+}
+
+export default Slackgroups;
