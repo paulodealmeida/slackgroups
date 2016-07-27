d26db70d03b60ca5cd6b9a34bcfae9f58ef4f1a7
Pablo Bello <thefactus@gmail.com>
2016-07-27 17:36:27 -0300

    Add api controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,30 +1,32 @@
-class Api::V1::SessionsController < ApplicationController
-  def create
-    user = User.find_by(email: create_params[:email])
-    if user.valid_password?(create_params[:password])
-      sign_in(user, store: false)
-      user.generate_authentication_token!
-      user.save
-      render json: user, status: :created, location: [:api, user]
-    else
-      render json: { errors: 'Invalid email or password' }, status: :unprocessable_entity
+module Api::V1
+  class SessionsController < ApiController
+    def create
+      user = User.find_by(email: create_params[:email])
+      if user.valid_password?(create_params[:password])
+        sign_in(user, store: false)
+        user.generate_authentication_token!
+        user.save
+        render json: user, status: :created, location: [:api, user]
+      else
+        render json: { errors: 'Invalid email or password' }, status: :unprocessable_entity
+      end
     end
-  end
 
-  def destroy
-    user = User.find_by(auth_token: params[:id])
-    if user
-      user.generate_authentication_token!
-      user.save
-      head 204
-    else
-      head 401
+    def destroy
+      user = User.find_by(auth_token: params[:id])
+      if user
+        user.generate_authentication_token!
+        user.save
+        head 204
+      else
+        head 401
+      end
     end
-  end
 
-  private
+    private
 
-  def create_params
-    params.require(:session).permit(:email, :password)
+    def create_params
+      params.require(:session).permit(:email, :password)
+    end
   end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,40 +1,42 @@
-class Api::V1::UsersController < ApplicationController
-  def show
-    user = find_user
-    render json: user
-  end
-
-  def create
-    user = User.new(user_params)
-    if user.save
-      render json: user, status: :created, location: [:api, user]
-    else
-      render json: { errors: user.errors }, status: :unprocessable_entity
+module Api::V1
+  class UsersController < ApiController
+    def show
+      user = find_user
+      render json: user
     end
-  end
 
-  def update
-    user = find_user
-    if user.update(user_params)
-      render json: user, status: :no_content, location: [:api, user]
-    else
-      render json: { errors: user.errors }, status: :unprocessable_entity
+    def create
+      user = User.new(user_params)
+      if user.save
+        render json: user, status: :created, location: [:api, user]
+      else
+        render json: { errors: user.errors }, status: :unprocessable_entity
+      end
     end
-  end
 
-  def destroy
-    user = find_user
-    user.destroy
-    head :no_content
-  end
+    def update
+      user = find_user
+      if user.update(user_params)
+        render json: user, status: :no_content, location: [:api, user]
+      else
+        render json: { errors: user.errors }, status: :unprocessable_entity
+      end
+    end
 
-  private
+    def destroy
+      user = find_user
+      user.destroy
+      head :no_content
+    end
 
-  def find_user
-    User.find(params[:id])
-  end
+    private
 
-  def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    def find_user
+      User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
   end
 end
