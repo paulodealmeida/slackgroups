ceddcd734bb16958ebeaf4e9a553b7c6f92c1b71
Pablo Bello <thefactus@gmail.com>
2016-07-27 11:35:43 -0300

    Define api version on controllers spec

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Api::V1::UsersController, type: :controller do
-  before(:each) { request.headers['Accept'] = 'application/vnd.marketplace.v1' }
+  before(:each) { request.headers['Accept'] = 'application/vnd.slackgroups.v1' }
 
   describe 'GET #show' do
     before(:each) do
