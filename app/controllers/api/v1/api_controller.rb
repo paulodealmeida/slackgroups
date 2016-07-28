0a64d5e8b8af19db0e5f664521c036ebea9bf239
Pablo Bello <thefactus@gmail.com>
2016-07-27 22:45:09 -0300

    Add auth with token before action to api controller

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,2 +1,7 @@
-class Api::V1::ApiController < ApplicationController
+module Api
+  module V1
+    class ApiController < ApplicationController
+      before_action :auth_with_token!
+    end
+  end
 end
