4e24600a866ef743724fa8aa74a3eb888222bf10
fabionobre <eu@fabionobre.com.br>
2016-07-29 13:36:55 -0300

    Change Groups header to work in with cross-browser.

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -1,66 +1,13 @@
-<<<<<<< HEAD
-class Api::V1::GroupsController < ApplicationController
-
-  before_filter :add_allow_credentials_headers, only: [:index, :show, :create]
-
-  def index
-    groups = Group.all.order('title asc')
-    render json: groups, callback: params[:callback]
-  end
-
-  def show
-    group = find_group
-    render json: group
-  end
-
-  def create
-    group = Group.new(group_params)
-
-    if group.save
-      render json: group, status: :created, callback: params[:callback], :'Access-Control-Allow-Headers' => 'accept, content-type'
-    else
-      render json: { errors: group.errors }, status: :unprocessable_entity
-    end
-  end
-
-  def update
-    group = find_group
-    if group.update(group_params)
-      render json: group, status: :no_content
-    else
-      render json: { errors: group.errors }, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    group = find_group
-    group.destroy
-    head :no_content
-  end
-
-  private
-
-  def find_group
-    Group.find(params[:id])
-  end
-
-  def group_params
-    params.require(:group).permit(:title, :description, :url)
-  end
-
-  def add_allow_credentials_headers
-    response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || '*'                                                                                                                                                                                                     
-    response.headers['Access-Control-Allow-Credentials'] = 'true'  
-  end
-=======
 module Api
   module V1
     class GroupsController < ApiController
+
       skip_before_action :auth_with_token!
+      before_filter :add_allow_credentials_headers, only: [:index, :show, :create]
 
       def index
         groups = Group.all
-        render json: groups, callback: params[:callback]
+        render json: groups
       end
 
       def show
@@ -102,7 +49,11 @@ module Api
       def group_params
         params.require(:group).permit(:title, :description, :url)
       end
+      
+      def add_allow_credentials_headers
+        response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || '*'                                                                                                                                                                                                     
+        response.headers['Access-Control-Allow-Credentials'] = 'true'  
+      end
     end
   end
->>>>>>> paulodealmeida/master
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,5 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
-<<<<<<< HEAD
-  protect_from_forgery with: :null_session
-=======
-
   include Concerns::Authenticable
->>>>>>> paulodealmeida/master
 end
