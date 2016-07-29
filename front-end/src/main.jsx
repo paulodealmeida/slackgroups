71b8e2dd9488cd45c179729684321ad315adafca
fabionobre <eu@fabionobre.com.br>
2016-07-29 13:35:38 -0300

    Create Main Page in ReactJS.

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,5 +1,5 @@
-import Slackgroups from './slackgroups';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Slackgroups from './slackgroups';
 
 ReactDOM.render(<Slackgroups />, document.querySelector('#view'));
