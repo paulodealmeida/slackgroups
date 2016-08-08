2ee27b022d5a710461c406537d7b9c3d8441db10
Pablo Bello <thefactus@gmail.com>
2016-08-08 15:02:36 -0300

    Fix indentation

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -1,7 +1,6 @@
 module Api
   module V1
     class GroupsController < ApiController
-
       skip_before_action :auth_with_token!
       before_filter :add_allow_credentials_headers, only: [:index, :show, :create]
 
@@ -16,7 +15,6 @@ module Api
       end
 
       def create
-
         group = Group.new(group_params_json)
 
         if group.save
@@ -50,14 +48,14 @@ module Api
       def group_params
         params.require(:group).permit(:title, :description, :url)
       end
- 
-       def group_params_json
+
+      def group_params_json
         JSON.parse(params[:group])
       end
 
       def add_allow_credentials_headers
-        response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || '*'                                                                                                                                                                                                     
-        response.headers['Access-Control-Allow-Credentials'] = 'true'  
+        response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || '*'
+        response.headers['Access-Control-Allow-Credentials'] = 'true'
       end
     end
   end
