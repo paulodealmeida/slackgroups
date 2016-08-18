3ac1d19cec274d1857ca5132fc300ab4f115bbea
Pablo Bello <thefactus@gmail.com>
2016-08-18 19:14:57 -0300

    Remove devise for users

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ require 'api_constraints'
 
 Rails.application.routes.draw do
 
-  devise_for :users
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :groups
