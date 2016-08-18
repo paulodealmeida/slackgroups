d82168cc31ee4fb09a2076f98b1b528a3bb5df6b
Pablo Bello <thefactus@gmail.com>
2016-08-18 19:19:08 -0300

    Fix identation and remove white spaces

--- front-end/gulpfile.js
+++ front-end/gulpfile.js
@@ -9,7 +9,7 @@
   var sass = require('gulp-sass');
   var sourcemaps = require('gulp-sourcemaps');
   var babel = require('gulp-babel');
-  
+
   var appRoot = './src/';
   var outputRoot = '../public/script';
   var cssRoot = '../public/css'
